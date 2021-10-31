84b7fda06e588e380b205089dc3831ac522870d0
kaneko <kane4108@yomiuri.com>
2021-10-31 15:10:16 +0900

    add: ハンドラ設定関数追加

--- log.py
+++ log.py
@@ -16,3 +16,17 @@ class Log:
         logger.addHandler(handler)
 
         return logger
+
+    def setHandler(self) -> object:
+
+        """
+        ハンドラーに各設定を追加
+        """
+
+        handler = logging.StreamHandler()
+        #出力するログの最低レベルを設定
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s %(module)s Line:%(lineno)s\n%(message)s')
+        handler.setFormatter(formatter)
+
+        return handler
